c643d27ccf198ad604465eebab67ca80f07afdc5
BizmateCL <pmonsalve@bizmate.cl>
2025-06-08 21:11:49 -0400

    se sube proyecto a Railway y se configura la bd en MongoDB Cloud Atlas

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,9 @@
 const mongoose= require('mongoose');
 
-
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);//obtenemos el valor a traves de variables de entorno
-        console.log('Conectado a la base de atos');
+        console.log('Conectado a la base de datos');
     } catch (error) {
         console.error('Error al conectar con MongoDB:', error.message);
         process.exit(1); // en caso de error con la bd
